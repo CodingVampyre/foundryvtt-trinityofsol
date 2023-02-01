24a64de3132c48830657c804b34753625d365871
Tobias Kavsek <ictao-trash@protonmail.com>
2023-02-01 22:17:20 +0100

    Initial commit

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const TRINITYOFSOL = {};
+
+/**
+ * The set of Ability Scores used within the sytem.
+ * @type {Object}
+ */
+ TRINITYOFSOL.abilities = {
+  "str": "TRINITYOFSOL.AbilityStr",
+  "dex": "TRINITYOFSOL.AbilityDex",
+  "con": "TRINITYOFSOL.AbilityCon",
+  "int": "TRINITYOFSOL.AbilityInt",
+  "wis": "TRINITYOFSOL.AbilityWis",
+  "cha": "TRINITYOFSOL.AbilityCha"
+};
+
+TRINITYOFSOL.abilityAbbreviations = {
+  "str": "TRINITYOFSOL.AbilityStrAbbr",
+  "dex": "TRINITYOFSOL.AbilityDexAbbr",
+  "con": "TRINITYOFSOL.AbilityConAbbr",
+  "int": "TRINITYOFSOL.AbilityIntAbbr",
+  "wis": "TRINITYOFSOL.AbilityWisAbbr",
+  "cha": "TRINITYOFSOL.AbilityChaAbbr"
+};
